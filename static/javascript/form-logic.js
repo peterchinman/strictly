2c7e2b0191386944b871052f546800a323cbc1ab
Peter Chinman <peter.chinman@gmail.com>
2024-10-22 11:40:49 -0400

    initial commit

---
+++ static/javascript/form-logic.js
@@ -0,0 +1,8 @@
+var createYourOwn = document.getElementById('create-your-own');
+var customFormBoolean = false;
+
+createYourOwn.addEventListener('click', function() {
+   console.log("Button pressed");
+   createYourOwn.classList.toggle('active');
+   
+})
