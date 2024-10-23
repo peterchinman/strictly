02d62bc7d4c745e1dbe920bc5c4ffaf3230670c2
Peter Chinman <peter.chinman@gmail.com>
2024-10-23 18:55:29 -0400

    set up poetry creation form, initial form-logic

--- static/javascript/form-logic.js
+++ static/javascript/form-logic.js
@@ -1,8 +1,74 @@
-var createYourOwn = document.getElementById('create-your-own');
+var createYourOwnButton = document.getElementById('create-your-own');
 var customFormBoolean = false;
 
-createYourOwn.addEventListener('click', function() {
-   console.log("Button pressed");
-   createYourOwn.classList.toggle('active');
-   
+var strictLines = document.getElementById('strict-lines');
+var repeatingStanzas = document.getElementById('repeating-stanzas');
+var strictRhyme = document.getElementById('strict-rhyme');
+var strictSyllable = document.getElementById('strict-syllable');
+var strictMeter = document.getElementById('strict-meter')
+
+
+createYourOwnButton.addEventListener('click', function() {
+   createYourOwnButton.classList.toggle('active');
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+   const strictCheckboxes = document.querySelectorAll('input[type=checkbox][name=strict-checkbox]');
+
+   var strictOptionArray = []
+
+   strictCheckboxes.forEach(function(checkbox) {
+      // record initial states
+      
+      checkbox.addEventListener('change', function() {
+         strictOptionArray =
+            Array.from(strictCheckboxes)
+            .filter(i => i.checked)
+            .map(i => i.value);
+         console.log(strictOptionArray);
+         
+         if (strictOptionArray.includes("strict-lines")) {
+            // include number of lines input
+            console.log("strict lines checked")
+         }
+   
+         if (strictOptionArray.includes("repeating-stanzas")) {
+            // include stanza line length input
+
+            // needs to check that number of lines is a multiple of stanza line length
+
+            // include stanzas contain differing lines slider
+
+            //if that is checked need to populate line options for the number of lines
+         }
+
+         if (strictOptionArray.includes("repeating-stanzas") || strictOptionArray.includes("strict-lines")) {
+            // enable strict rhyme
+            strictRhyme.disabled = false;
+            
+         }
+         else {
+            strictRhyme.disabled = true;
+         }
+
+   
+         if (strictOptionArray.includes("strict-rhyme")) {
+            // include rhyme schme input
+            // also should indicate what strict rhyme is applying to
+         }
+
+         if (strictOptionArray.includes("strict-meter")) {
+            // if NOT differing lines
+            // include meter input
+         }
+   
+      })
+   })
+
+})
+
+
+
+
+
+
